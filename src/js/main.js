e51a64f7622ffd5444a3599fb897df41daf26f5e
maquarocket <76931815+maquarocket@users.noreply.github.com>
2026-01-26 12:32:53 +0800

    Added quick instructions. Created stylesheet.

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,5 @@ import Suit from "./Suit.js";
 import Value from "./Value.js";
 import Hand from "./Hand.js";
 
+document.querySelector('.hide-quick-instructions').addEventListener('click', () => {document.querySelector('.quick-instructions').setAttribute('hidden', '')});
+document.querySelector('.show-quick-instructions').addEventListener('click', () => {document.querySelector('.quick-instructions').removeAttribute('hidden')});
